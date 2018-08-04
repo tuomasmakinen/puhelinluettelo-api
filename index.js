95eb2aa48b8c03c4d36abbcd9a6e91eb3eb28b8b
Tuomas Mäkinen <tuomas.makinen2@centria.fi>
2018-08-05 00:27:05 +0300

    3.4

--- index.js
+++ index.js
@@ -39,6 +39,13 @@ app.get( '/api/persons/:id', ( request, response ) => {
 	}
 } );
 
+app.delete( '/api/persons/:id', ( request, response ) => {
+	const id = Number( request.params.id );
+	persons = persons.filter( person => person.id !== id );
+
+	response.status( 204 ).end();
+} );
+
 app.get( '/info', ( request, response ) => {
 	let body = `<p>Puhelinluettelossa on ${ persons.length } henkilön tiedot</p><br/><p>${ new Date() }</p>`;
 	response.send( body );
