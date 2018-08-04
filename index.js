6879f4d362854335ec28b52c51aac327c299ef1f
Tuomas Mäkinen <tuomas.makinen2@centria.fi>
2018-08-05 01:38:49 +0300

    3.7 & 3.8

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require( 'express' );
-const app = express();
 const bodyParser = require( 'body-parser' );
+const morgan = require( 'morgan' );
+
+morgan.token( 'body', ( request, response ) => {
+	return JSON.stringify( request.body );
+} );
+
+const app = express();
 
 app.use( bodyParser.json() );
+app.use( morgan( ':method :url :body :status :res[content-length] - :response-time ms' ) );
 
 let persons = [
 	{
