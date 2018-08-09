58d0395518007103b949bbccae38c045bb9ab5f4
Tuomas Mäkinen <tuomas.makinen2@centria.fi>
2018-08-09 18:50:46 +0300

    3.16

--- index.js
+++ index.js
@@ -59,10 +59,14 @@ app.get( '/api/persons/:id', ( request, response ) => {
 } );
 
 app.delete( '/api/persons/:id', ( request, response ) => {
-	const id = Number( request.params.id );
-	persons = persons.filter( person => person.id !== id );
-
-	response.status( 204 ).end();
+	Person
+		.findByIdAndRemove( request.params.id )
+		.then( result => {
+			response.status( 204 ).end();
+		} )
+		.catch( error => {
+			response.status( 400 ).send({ error: 'malformed id' });
+		} );
 } );
 
 app.post( '/api/persons', ( request, response ) => {
