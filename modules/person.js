f92f90444803fb8a5d9f066e35b7c5e4810f444f
Tuomas Mäkinen <tuomas.makinen2@centria.fi>
2018-08-11 10:14:34 +0300

    3.21

--- modules/person.js
+++ modules/person.js
@@ -1,6 +1,10 @@
 const mongoose = require( 'mongoose' );
 const Schema = mongoose.Schema;
 
+if ( process.env.NODE_ENV !== 'production' ) {
+	require( 'dotenv' ).config();
+}
+
 const url = process.env.MONGODB_URI;
 
 mongoose.connect( url, { useNewUrlParser: true } );
